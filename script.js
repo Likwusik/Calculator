da5e93be66eaf578c15d8b2d3e90cf73ace48bd2
Likwusik <angelimakaro@gmail.com>
2024-04-03 14:28:29 +0200

    Add logic and style

---
+++ script.js
@@ -0,0 +1,62 @@
+import { sum, subtract, multiply, divide } from "./calculator.js";
+
+let output = document.querySelector("#output");
+
+let firstNumber = "";
+let operation = null;
+let resetAfterOperation = false;
+
+document.querySelectorAll("#calculator .number").forEach((button) => {
+  button.addEventListener("click", (event) => {
+    let value = event.currentTarget.textContent;
+    if (resetAfterOperation) {
+      output.value = value;
+      resetAfterOperation = false;
+    } else {
+      output.value += value;
+    }
+  });
+});
+
+document.querySelectorAll("#calculator .operation").forEach((button) => {
+  button.addEventListener("click", (event) => {
+    firstNumber = output.value;
+    operation = event.currentTarget.dataset.action;
+    resetAfterOperation = true;
+  });
+});
+
+const equal = document.querySelector("#calculator .equal");
+equal.addEventListener("click", () => {
+  if (!operation) {
+    return;
+  }
+  resetAfterOperation = true;
+  let secondNumber = output.value;
+
+  let result;
+  switch (operation) {
+    case "sum":
+      result = sum(firstNumber, secondNumber);
+      break;
+    case "subtract":
+      result = subtract(firstNumber, secondNumber);
+      break;
+    case "multiply":
+      result = multiply(firstNumber, secondNumber);
+      break;
+    case "divide":
+      if (secondNumber === 0) {
+        result = "Error: Division by zero";
+      } else {
+        result = divide(firstNumber, secondNumber);
+      }
+      break;
+    default:
+      result = "Error: Unknown operation";
+      break;
+  }
+  output.value = result;
+  //reset operation
+  operation = null;
+});
